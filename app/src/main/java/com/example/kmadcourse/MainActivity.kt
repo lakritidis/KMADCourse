69cf5e292bf744ad8459d0c3291f7f61ba6ea304
lakritidis <lakritidis@gmail.com>
2026-03-07 09:09:26 +0200

    via AndroidStudio

--- app/src/main/java/com/example/kmadcourse/MainActivity.kt
+++ app/src/main/java/com/example/kmadcourse/MainActivity.kt
@@ -17,44 +17,8 @@ class MainActivity : ComponentActivity() {
         val view = binding.root
         setContentView(view)
 
-        Toast.makeText(this, "App started successfully!", Toast.LENGTH_LONG).show()
-
-
-        val btnSubmit = binding.btnSubmit
-        btnSubmit.setOnClickListener {
-            Toast.makeText(this,"Button Pressed",Toast.LENGTH_LONG).show()
-        }
-
-        val et = binding.editSurname
-        et.setText(R.string.cap_hello)
-        // et.setEnabled(false)
-        et.visibility = View.VISIBLE
-        et.textSize = 20.0F
-        // et.setTextColor(Color.parseColor("#bb2539"))
-        et.setTextColor("#bb2539".toColorInt())
-        et.setBackgroundColor(Color.GRAY)
-
-        val ch = binding.chRemember
-        ch.isChecked = true
-
-        val rd = binding.rdLarge
-        rd.isChecked = true
-
-        // An onLongClickListener for our button
-        btnSubmit.setOnLongClickListener {
-            val uSurname = binding.editSurname
-            val currentSurname = uSurname.text.toString()
-
-            val uFirstname = binding.editFirstname
-            val currentFirstname = uFirstname.text.toString()
-
-            val uRemember = binding.chRemember
-            val currentRem = uRemember.isChecked.toString()
-
-            val data = "$currentSurname $currentFirstname: $currentRem"
-            Toast.makeText(this,data,Toast.LENGTH_LONG).show()
-            true
-        }
-
+        val btn = binding.myfirstButton
+        val btn2 = binding.mysecondButton
+        btn?.setText("I was changed!")
     }
 }
